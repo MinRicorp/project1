60b9f4f47eeaf884d4929ee3df9a05b998bfa615
unknown <sunzone81@gmail.com>
2018-10-14 17:41:13 +0900

    commit

--- vision_extension.js
+++ vision_extension.js
@@ -1,41 +1,37 @@
 (function(ext) {
-3      // Cleanup function when the extension is unloaded
-4      ext._shutdown = function() {};
-5
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
 
-6      // Status reporting code
-7      // Use this to report missing hardware, plugin or unsupported browser
-8      ext._getStatus = function() {
-9          return {status: 2, msg: 'Ready'};
-10      }; 
-11
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
 
-12      ext.getImageDescription = function(image, callback) {
-13        var imgUri = "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001003/Beagle-MP.jpg"
-14        var str = '{"requests":[{"image":{"source":{"imageUri":"' + imgUri + '"}},"features":[{"type":"LABEL_DETECTION","maxResults":3}]}]}'
-15        var request = new XMLHttpRequest();
-16        request.onreadystatechange= function () {
-17            if (request.readyState==4) {
-18                //handle response
-19                callback(this.responseText);
-20            }
-21        }
-22        request.open("POST", "https://vision.googleapis.com/v1/images:annotate?alt=json&key=API_KEY", true);
-23        request.setRequestHeader('Content-Type', 'application/json');
-24        request.send(str);
-25      };
-26
+    ext.getImageDescription = function(image, callback) {
+      var imgUri = "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001003/Beagle-MP.jpg"
+      var str = '{"requests":[{"image":{"source":{"imageUri":"' + imgUri + '"}},"features":[{"type":"LABEL_DETECTION","maxResults":3}]}]}'
+      var request = new XMLHttpRequest();
+      request.onreadystatechange= function () {
+          if (request.readyState==4) {
+              //handle response
+              callback(this.responseText);
+          }
+      }
+      request.open("POST", "https://vision.googleapis.com/v1/images:annotate?alt=json&key=API_KEY", true);
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.send(str);
+    };
 
-27      // Block and block menu descriptions
-28      var descriptor = {
-29          blocks: [
-30              // Block type, block name, function name, param1 default value, param2 default value
-31              ['R', 'image : %s', 'getImageDescription', 'image'],
-32          ],
-33          displayName: 'google Vision API Block'
-34      };
-35
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name, param1 default value, param2 default value
+            ['R', 'image : %s', 'getImageDescription', 'image'],
+        ],
+        displayName: 'google Vision API Block'
+    };
 
-36      // Register the extension
-37      ScratchExtensions.register('google Vision API sample', descriptor, ext);
-38  })({});
+    // Register the extension
+    ScratchExtensions.register('google Vision API sample', descriptor, ext);
+})({});
